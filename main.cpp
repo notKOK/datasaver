6db302c937baca5fd1a80a3be49b0405a9a032f7
Danila <a.deni.i@mail.ru>
2022-10-03 17:25:21 +0300

    insert to db

--- main.cpp
+++ main.cpp
@@ -2,26 +2,28 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <pqxx/pqxx>
 
 using namespace std;
+using namespace pqxx;
 
 class flights {
     int64_t APMtime;
     int64_t GPStime;
     unsigned int flight_number;
 public:
-    void readdata(FILE *file);
+    void readData(FILE *file);
+    static void insertData();
 };
 
-void flights::readdata(FILE *file) {
+void flights::readData(FILE *file) {
     fseek(file, 33, SEEK_SET);
     fread(&this->APMtime, 8, 1, file);
     fread(&this->GPStime, 8, 1, file);
     fseek(file, 4, SEEK_CUR);
     fread(&this->flight_number, 4, 1, file);
-    this->APMtime /= 1000;
+    this->APMtime /= 1000; //convert to seconds
     this->GPStime /= 1000;
-
     //convert time to another form
    /* struct tm tm = *localtime(&this->APMtime);
     printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
@@ -31,8 +33,37 @@ void flights::readdata(FILE *file) {
            tm.tm_hour, tm.tm_min, tm.tm_sec);*/
 }
 
+void flights::insertData() {
+    char const *sql;
 
-int fileCheck(FILE *file);
+    try {
+        connection C("dbname = datasaver user = keeper password = '' hostaddr = 127.0.0.1 port = 5432");
+        if (C.is_open()) {
+            cout << "Opened database successfully: " << C.dbname() << endl;
+        } else {
+            cout << "Can't open database" << endl;
+            return;
+        }
+
+        /* Create SQL statement */
+        sql = "INSERT INTO flights (NumFly) " \
+              "VALUES (19)";
+
+        /* Create a transactional object. */
+        work W(C);
+
+        /* Execute SQL query */
+        W.exec( sql );
+        W.commit();
+        cout << "Records created successfully" << endl;
+    } catch (const std::exception &e) {
+        cerr << e.what() << std::endl;
+        return;
+    }
+}
+
+
+void fileCheck(FILE *file);
 
 int main() {
     FILE *pFile;
@@ -42,13 +73,14 @@ int main() {
     if(pFile == nullptr) perror ("Error opening file");
 
     flights test{};
-    test.readdata(pFile);
+    test.readData(pFile);
+    test.insertData();
     fclose(pFile);
     cout << "OK " << endl;
     return 0;
 }
 
-int fileCheck(FILE *file){
+void fileCheck(FILE *file){
     if(file == nullptr) perror ("file == nullptr");
     vector<unsigned int>checkvec { 0x00FF00FF, 0x01FC01FE, 0x01F001F8, 0x56AA55AA };
     size_t checkvecsize = checkvec.size();
